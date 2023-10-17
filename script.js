cd12d257bd0579fa91eaf20c40ead083c5377732
Carmen Morata <cmoratagarcia@gmail.com>
2023-10-17 09:46:45 -0700

    Add error for out of range numbers

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 const container = document.querySelector(".container");
 let divString = prompt("Please enter a number between 0 and 100");
-//Create function that takes user input and converts it to div grid
 
 //Initialize function at 16 * 16
+//Create function that takes user input and converts it to div grid
 function createGrid(string) {
   //Convert user input to number and make it square
   let toNumber = Number(string);
+  //Error if number is higher that 100
+  if (toNumber > 100 || toNumber < 0) {
+    //Add error if NaN
+    alert("Wrong value!");
+    return;
+  }
   let divQtty = toNumber * toNumber;
 
   for (let i = 0; i < divQtty; i++) {
